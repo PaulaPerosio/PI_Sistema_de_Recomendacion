5c9b11e40cea83f92cb0dfb66f3147029a9eacfe
Paula Perosio <perosiopaula@gmail.com>
2023-09-17 21:59:37 -0300

    4

--- main.py
+++ main.py
@@ -21,6 +21,41 @@ async def ruta_prueba_2(id:int):
 async def ruta_prueba_2(id:int):
     return {"HolAAAAAAAAAAAa": id}
 
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+@app.get("/countreviews/")
+async def countReviews(fecha_inicio_str: str, fecha_fin_str: str):
+    '''Esta funcion devuelve la cantidad de usuarios que realizaron reviews entre las fechas dadas y,
+    el porcentaje de recomendación de los mismos en base a reviews.recommend'''
+
+    try:
+        df = pd.read_json("DataSets/df_countreviews.json", lines=True)
+    except Exception as e:
+        return {"error": f"No se pudo leer el archivo JSON. Detalles: {e}"}
+
+    if not fecha_inicio_str.count('/') == fecha_fin_str.count('/') == 2:
+        return {"error": "El formato de fecha es incorrecto. Debe ser YYYY/MM/DD."}
+
+    partes_inicio = fecha_inicio_str.split('/')
+    partes_fin = fecha_fin_str.split('/')
+
+    if not all(part.isdigit() for part in partes_inicio + partes_fin):
+        return {"error": "Las fechas deben contener solo números."}
+
+    if not (len(partes_inicio[0]) == len(partes_fin[0]) == 4 and 
+            len(partes_inicio[1]) == len(partes_fin[1]) == 2 and 
+            len(partes_inicio[2]) == len(partes_fin[2]) == 2):
+        return {"error": "Las fechas deben estar en el formato YYYY/MM/DD."}
+
+    condicion = (df['posted_ok'] >= fecha_inicio_str) & (df['posted_ok'] <= fecha_fin_str)
+    recomendacion = df[condicion][df[condicion]['recommend']==True].shape[0]
+    porc_recomendacion = recomendacion / df[condicion].shape[0] * 100
+
+    return {"Cantidad de usuarios": df[condicion].shape[0], "Porcentaje de recomendación": round(porc_recomendacion, 2)}
+
+
+
 @app.get("/sentiment_analysis/{año}")
 async def sentiment_analysis(año:int):
     
